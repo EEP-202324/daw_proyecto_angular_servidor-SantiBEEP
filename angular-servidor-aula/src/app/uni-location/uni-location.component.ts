f71cef16df0316afab3143a5e44489e373e8e42f
SantiBEEP <Santiago-Badajoz1@eep-igroup.com>
2024-05-10 20:40:53 +0200

    Creo el componente del Location de las Universidades

---
+++ angular-servidor-aula/src/app/uni-location/uni-location.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uni-location',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      uni-location works!
+    </p>
+  `,
+  styleUrl: './uni-location.component.css'
+})
+export class UniLocationComponent {
+
+}
--- angular-servidor-aula/src/app/universidad/universidad.component.ts
+++ angular-servidor-aula/src/app/universidad/universidad.component.ts
@@ -1,15 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { UniLocationComponent } from '../uni-location/uni-location.component';
 
 @Component({
   selector: 'app-universidad',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    UniLocationComponent],
   template: `
   <section>
     <form>
       <input type="text" placeholder="Filtrar Universidades">
       <button class="primary" type="button">Buscar</button>
     </form>
+  </section>
+  <section class="results">
+  <app-uni-location></app-uni-location>
   </section>
 `,
   styleUrl: './universidad.component.css'

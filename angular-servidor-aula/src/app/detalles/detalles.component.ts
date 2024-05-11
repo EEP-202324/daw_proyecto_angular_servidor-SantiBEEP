72e789d21ada62f0790da96fe344197bb9c0bbad
SantiBEEP <Santiago-Badajoz1@eep-igroup.com>
2024-05-11 20:14:25 +0200

    Añado los detalles de cada Universidad

--- angular-servidor-aula/src/app/detalles/detalles.component.ts
+++ angular-servidor-aula/src/app/detalles/detalles.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { UniService } from '../uni.service';
+import { UniLocation } from '../uni-location';
 
 @Component({
   selector: 'app-detalles',
   standalone: true,
   imports: [],
   template: `
-    <p>
-      detalles works!
-    </p>
+  <article>
+  <img class="listing-photo" [src]="uniLocation?.photo"
+    alt="Exterior photo of {{uniLocation?.name}}"/>
+  <section class="listing-description">
+    <h2 class="listing-heading">{{uniLocation?.name}}</h2>
+    <p class="listing-location">{{uniLocation?.city}}</p>
+  </section>
+  <section class="listing-features">
+    <h2 class="section-heading">Sobre esta Universidad</h2>
+    <ul>
+      <li>¡Por hacer!</li>
+    </ul>
+  </section>
+</article>
   `,
   styleUrl: './detalles.component.css'
 })
 export class DetallesComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  uniService = inject(UniService);
+  uniLocation: UniLocation | undefined;
 
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.uniLocation = this.uniService.getUniLocationById(housingLocationId);
+  }
 }
--- angular-servidor-aula/src/app/uni-location/uni-location.component.ts
+++ angular-servidor-aula/src/app/uni-location/uni-location.component.ts
@@ -1,16 +1,21 @@
 import { Component, Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UniLocation } from '../uni-location';
+import { RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-uni-location',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,
+    RouterOutlet
+  ],
   template: `
   <section class="listing">
     <img class="listing-photo" [src]="uniLocation.photo" alt="Exterior photo of {{uniLocation.name}}">
     <h2 class="listing-heading">{{ uniLocation.name }}</h2>
     <p class="listing-location">{{ uniLocation.city}}</p>
+    <a [routerLink]="['/detalles', uniLocation.id]">Saber más</a>
   </section>
   `,
   styleUrl: './uni-location.component.css'

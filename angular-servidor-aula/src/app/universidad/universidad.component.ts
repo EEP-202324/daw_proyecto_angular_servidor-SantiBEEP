43ebb14e92542f012358a45aaebc8d0ceaa57ce0
SantiBEEP <Santiago-Badajoz1@eep-igroup.com>
2024-05-11 15:24:34 +0200

    añado ngFor para listar las universidades

--- angular-servidor-aula/src/app/universidad/universidad.component.ts
+++ angular-servidor-aula/src/app/universidad/universidad.component.ts
@@ -8,7 +8,8 @@ import { UniLocation } from '../uni-location';
   standalone: true,
   imports: [
     CommonModule,
-    UniLocationComponent],
+    UniLocationComponent
+    ],
   template: `
   <section>
     <form>
@@ -17,7 +18,8 @@ import { UniLocation } from '../uni-location';
     </form>
   </section>
   <section class="results">
-  <app-uni-location [uniLocation] = "UniLocation"></app-uni-location>
+  <app-uni-location *ngFor="let uniLocation of uniLocationList"
+  [uniLocation]="uniLocation"></app-uni-location>
   </section>
 `,
   styleUrl: './universidad.component.css'
@@ -25,10 +27,66 @@ import { UniLocation } from '../uni-location';
 export class UniversidadComponent {
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
-  UniLocation: UniLocation = {
-    id: 9999,
-    name: 'Universidad1',
-    city: 'Madrid',
-    photo: `${this.baseUrl}/example-house.jpg`,
-  };
+  uniLocationList: UniLocation[] = [
+    {
+      id: 0,
+      name: 'Acme Fresh Start Housing',
+      city: 'Madrid',
+      photo: `${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg`,
+    },
+    {
+      id: 1,
+      name: 'A113 Transitional Housing',
+      city: 'Madrid',
+      photo: `${this.baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg`,
+    },
+    {
+      id: 2,
+      name: 'Warm Beds Housing Support',
+      city: 'Madrid',
+      photo: `${this.baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg`,
+    },
+    {
+      id: 3,
+      name: 'Homesteady Housing',
+      city: 'Madrid',
+      photo: `${this.baseUrl}/ian-macdonald-W8z6aiwfi1E-unsplash.jpg`,
+    },
+    {
+      id: 4,
+      name: 'Happy Homes Group',
+      city: 'Madrid',
+      photo: `${this.baseUrl}/krzysztof-hepner-978RAXoXnH4-unsplash.jpg`,
+    },
+    {
+      id: 5,
+      name: 'Hopeful Apartment Group',
+      city: 'Madrid',
+      photo: `${this.baseUrl}/r-architecture-JvQ0Q5IkeMM-unsplash.jpg`,
+    },
+    {
+      id: 6,
+      name: 'Seriously Safe Towns',
+      city: 'Madrid',
+      photo: `${this.baseUrl}/phil-hearing-IYfp2Ixe9nM-unsplash.jpg`,
+    },
+    {
+      id: 7,
+      name: 'Hopeful Housing Solutions',
+      city: 'Madrid',
+      photo: `${this.baseUrl}/r-architecture-GGupkreKwxA-unsplash.jpg`,
+    },
+    {
+      id: 8,
+      name: 'Seriously Safe Towns',
+      city: 'Madrid',
+      photo: `${this.baseUrl}/saru-robert-9rP3mxf8qWI-unsplash.jpg`,
+    },
+    {
+      id: 9,
+      name: 'Capital Safe Towns',
+      city: 'Madrid',
+      photo: `${this.baseUrl}/webaliser-_TPTXZd9mOo-unsplash.jpg`,
+    }
+  ];
 }

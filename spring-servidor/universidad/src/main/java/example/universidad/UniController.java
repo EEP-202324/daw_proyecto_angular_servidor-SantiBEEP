0aed65c3f73bdd58c450f8120e2a672fd225f582
SantiBEEP <Santiago-Badajoz1@eep-igroup.com>
2024-05-17 21:12:21 +0200

    Añado el POST

--- spring-servidor/universidad/src/main/java/example/universidad/UniController.java
+++ spring-servidor/universidad/src/main/java/example/universidad/UniController.java
@@ -1,12 +1,16 @@
 package example.universidad;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/universidad")
@@ -25,5 +29,13 @@ class UniController {
 		} else {
 			return ResponseEntity.notFound().build();
 		}
+	}
+	
+	@PostMapping
+	private ResponseEntity<Void> createUni(@RequestBody Universidad newUniRequest, UriComponentsBuilder ucb) {
+		Universidad savedUni = universidadRepository.save(newUniRequest);
+		URI locationOfNewUni = ucb.path("universidad/{id}").buildAndExpand(savedUni.id()).toUri();
+		return ResponseEntity.created(locationOfNewUni).build();
+
 	}
 }
--- spring-servidor/universidad/src/test/java/example/universidad/UniversidadApplicationTests.java
+++ spring-servidor/universidad/src/test/java/example/universidad/UniversidadApplicationTests.java
@@ -1,7 +1,9 @@
 package example.universidad;
 
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +11,8 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UniversidadApplicationTests {
@@ -38,5 +41,23 @@ class UniversidadApplicationTests {
 
       assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
       assertThat(response.getBody()).isBlank();
+    }
+    
+    @Test
+    void shouldCreateANewUni() {
+       Universidad newUni = new Universidad(null, "uni2");
+       ResponseEntity<Void> createResponse = restTemplate.postForEntity("/universidad", newUni, Void.class);
+       assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+       
+       URI locationOfNewUni = createResponse.getHeaders().getLocation();
+       ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewUni, String.class);
+       assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+       
+       DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+       Number id = documentContext.read("$.id");
+       String name = documentContext.read("$.name");
+
+       assertThat(id).isNotNull();
+       assertThat(name).isEqualTo("uni2");
     }
 }

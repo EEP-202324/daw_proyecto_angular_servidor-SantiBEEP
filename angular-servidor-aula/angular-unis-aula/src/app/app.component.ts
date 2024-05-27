07228a9ecfbf29196bac300ffd921c1afecaf71c
SantiBEEP <Santiago-Badajoz1@eep-igroup.com>
2024-05-27 19:05:51 +0200

    Creo componente Universidad

--- angular-servidor-aula/angular-unis-aula/src/app/app.component.ts
+++ angular-servidor-aula/angular-unis-aula/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UniversidadesComponent } from './universidades/universidades.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UniversidadesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ angular-servidor-aula/angular-unis-aula/src/app/universidades/universidades.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-universidades',
+  standalone: true,
+  imports: [],
+  templateUrl: './universidades.component.html',
+  styleUrl: './universidades.component.css'
+})
+export class UniversidadesComponent {
+  universidad = 'Universidad 1';
+}

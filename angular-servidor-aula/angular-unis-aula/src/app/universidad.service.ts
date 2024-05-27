d22d5a587644d0a351c50e3fccab0df66763ae44
SantiBEEP <Santiago-Badajoz1@eep-igroup.com>
2024-05-27 21:03:55 +0200

    get en el component de universidades

--- angular-servidor-aula/angular-unis-aula/src/app/universidad.service.ts
+++ angular-servidor-aula/angular-unis-aula/src/app/universidad.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Universidad } from './universidad';
+import { UNIVERSIDADES } from './mock-universidades';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,9 @@ import { Injectable } from '@angular/core';
 export class UniversidadService {
 
   constructor() { }
+
+  getUnis(): Observable<Universidad[]> {
+    const unis: Observable<Universidad[]> = of(UNIVERSIDADES)
+    return unis;
+  }
 }
--- angular-servidor-aula/angular-unis-aula/src/app/universidades/universidades.component.ts
+++ angular-servidor-aula/angular-unis-aula/src/app/universidades/universidades.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { UNIVERSIDADES } from '../mock-universidades';
 import { Universidad } from '../universidad';
+import { UniversidadService } from '../universidad.service';
 
 @Component({
   selector: 'app-universidades',
@@ -8,10 +8,19 @@ import { Universidad } from '../universidad';
   styleUrl: './universidades.component.css'
 })
 export class UniversidadesComponent {
-    universidades : Universidad[] = UNIVERSIDADES;
+    universidades : Universidad[] = [];
     uniSeleccionada?: Universidad;
+
+    getHeroes(): void {
+      this.universidadService.getUnis().subscribe(
+        unisRecibidas => this.universidades = unisRecibidas
+      );
+    }
 
     onSelect(universidad: Universidad): void {
      this.uniSeleccionada = universidad;
+  }
+  constructor(private universidadService: UniversidadService) {
+
   }
 }

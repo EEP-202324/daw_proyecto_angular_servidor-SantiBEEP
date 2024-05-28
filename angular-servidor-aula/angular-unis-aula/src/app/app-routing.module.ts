22f8484c03f8982ce8e5fbde7c993b52908ed163
SantiBEEP <Santiago-Badajoz1@eep-igroup.com>
2024-05-28 21:54:59 +0200

    Añado ruta a la lista de Unis

--- angular-servidor-aula/angular-unis-aula/src/app/app-routing.module.ts
+++ angular-servidor-aula/angular-unis-aula/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UniversidadesComponent } from './universidades/universidades.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'universidades', component: UniversidadesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

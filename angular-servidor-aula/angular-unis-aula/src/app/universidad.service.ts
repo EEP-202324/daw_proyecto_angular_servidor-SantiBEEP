2a19ca651dcdcbefbc1f53a52b47221769597157
SantiBEEP <Santiago-Badajoz1@eep-igroup.com>
2024-05-29 11:36:43 +0200

    los get con http.get y tap
    tap para logear

--- angular-servidor-aula/angular-unis-aula/src/app/universidad.service.ts
+++ angular-servidor-aula/angular-unis-aula/src/app/universidad.service.ts
@@ -4,6 +4,7 @@ import { UNIVERSIDADES } from './mock-universidades';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,17 +19,38 @@ export class UniversidadService {
     private messageService: MessageService) { }
 
   getUnis(): Observable<Universidad[]> {
-    this.messageService.add("UniversidadService: Universidades recuperadas");
-    return this.http.get<Universidad[]>(this.urlUnis);
+    return this.http.get<Universidad[]>(this.urlUnis).
+    pipe(
+      tap(_ => this.log('universidades recuperadas')),
+      catchError(this.handleError<Universidad[]>('getUnis', []))
+  );
   }
 
   getUni(id: number): Observable<Universidad> {
-    const uni = UNIVERSIDADES.find(u => u.id === id)!;
+    const url = `${this.urlUnis}/${id}`;
     this.messageService.add(`UniversidadService: Uni recuperada id=${id}`);
-    return of(uni);
+    return this.http.get<Universidad>(url).pipe(
+      tap(_ => this.log(`uni recuperada id=${id}`)),
+      catchError(this.handleError<Universidad>(`getUni id=${id}`))
+    );
   }
 
   private log(message: string) {
     this.messageService.add(`UniversidadService:${message}`)
   }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+  
 }
